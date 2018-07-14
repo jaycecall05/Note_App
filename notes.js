d7a45aeb9393cad6937f2e146685123825149c72
Jayce <callrj5@gmail.com>
2018-07-14 10:19:01 -0600

    adding and saving notes, also getting input from user

--- notes.js
+++ notes.js
@@ -1,7 +1,23 @@
 console.log('Staring notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+    
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 var getAll = () => {
