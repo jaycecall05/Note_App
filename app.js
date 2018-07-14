08486461f8f2717d8b254ce4a492a4f6de6f08e3
Jayce <callrj5@gmail.com>
2018-07-14 00:17:01 -0600

    starting note app

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,7 @@
+console.log('Staring notes.js');
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+};
+
