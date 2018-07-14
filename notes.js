52592cb4710bd851a19f7ccdd47868f30f8664f6
Jayce <callrj5@gmail.com>
2018-07-14 01:17:30 -0600

    added remove, add, read, get and not recognized functions

--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
 console.log('Staring notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Fetching note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Deleting Note', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
 
